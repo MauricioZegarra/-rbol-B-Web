a0d68a44fad6ed40d09f053087ac0e0ee16e99ea
Mauricio <mzegarrapu@unsa.edu.pe>
2024-07-12 09:19:39 -0500

    Trabajando con WebAssembly

--- BTree.cpp
+++ BTree.cpp
@@ -1,9 +1,7 @@
-#ifndef BTREE_H
-#define BTREE_H
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -25,9 +23,8 @@ struct Node {
 
     void serialize(ofstream& out) const;
     void deserialize(ifstream& in);
+    void to_dot(std::ostringstream& oss, int& nodeCounter) const;  // Añadido
 };
-
-/////////////////////////////////////////////////////////////
 
 // Node implementation
 template <class T, int Order>
@@ -39,29 +36,23 @@ Node<T, Order>::Node() {
 
 template <class T, int Order>
 int Node<T, Order>::Insert(T value) {
-    // if the node is leaf
     if (this->childs[0] == nullptr) {
         this->keys[++this->position] = value;
         ++this->NumbersOfKeys;
-        // arrange the keys array after putting new value in node
         for (int i = this->position; i > 0; i--) {
             if (this->keys[i] < this->keys[i - 1])
                 std::swap(this->keys[i], this->keys[i - 1]);
         }
-    } else {  // if the node is not leaf
-        // count to get place of child to put the value in it
+    } else {
         int i = 0;
         for (; i < this->NumbersOfKeys && value > this->keys[i];) {
             i++;
         }
-        // Check if the child is full to split it
         int check = this->childs[i]->Insert(value);
-        // if node full
         if (check) {
             T mid;
             int TEMP = i;
-            Node<T, Order>* newNode = split(this->childs[i], &mid);  // Splitted Node to store the values and child that greater than the midValue
-            // allocate midValue in correct place
+            Node<T, Order>* newNode = split(this->childs[i], &mid);
             for (; i < this->NumbersOfKeys && mid > this->keys[i];) {
                 i++;
             }
@@ -72,25 +63,22 @@ int Node<T, Order>::Insert(T value) {
             ++this->NumbersOfKeys;
             ++this->position;
 
-            // allocate newNode Splitted in the correct place
             int k;
             for (k = this->NumbersOfKeys; k > TEMP + 1; k--) this->childs[k] = this->childs[k - 1];
             this->childs[k] = newNode;
         }
     }
-    if (this->NumbersOfKeys == Order) return 1;  // to split it
+    if (this->NumbersOfKeys == Order) return 1;
     else return 0;
 }
 
 template <class T, int Order>
-Node<T, Order>* Node<T, Order>::split(Node* node, T* med)  // mid to store value of mid and use it in insert func
-{
+Node<T, Order>* Node<T, Order>::split(Node* node, T* med) {
     int NumberOfKeys = node->NumbersOfKeys;
     Node<T, Order>* newNode = new Node<T, Order>();
     int midValue = NumberOfKeys / 2;
     *med = node->keys[midValue];
     int i;
-    // take the values after mid value
     for (i = midValue + 1; i < NumberOfKeys; ++i) {
         newNode->keys[++newNode->position] = node->keys[i];
         newNode->childs[newNode->position] = node->childs[i];
@@ -102,16 +90,15 @@ Node<T, Order>* Node<T, Order>::split(Node* node, T* med)  // mid to store value
     newNode->childs[newNode->position + 1] = node->childs[i];
     node->childs[i] = nullptr;
 
-    --node->NumbersOfKeys;  // because we take mid value...
+    --node->NumbersOfKeys;
     --node->position;
     return newNode;
 }
 
 template <class T, int Order>
 void Node<T, Order>::Print() {
-    int height = this->getHeight();  // number of levels  -> log (n)
-    for (int i = 1; i <= height; ++i) {  // 50 levels maximum
-        // O(n)
+    int height = this->getHeight();
+    for (int i = 1; i <= height; ++i) {
         if (i == 1) PrintUtil(i, true);
         else PrintUtil(i, false);
         cout << endl;
@@ -121,7 +108,6 @@ void Node<T, Order>::Print() {
 
 template <class T, int Order>
 void Node<T, Order>::PrintUtil(int height, bool checkRoot) {
-    // to print all values in the level
     if (height == 1 || checkRoot) {
         for (int i = 0; i < this->NumbersOfKeys; i++) {
             if (i == 0) cout << "|";
@@ -141,9 +127,8 @@ void Node<T, Order>::PrintUtil(int height, bool checkRoot) {
 template <class T, int Order>
 int Node<T, Order>::getHeight() {
     int COUNT = 1;
-    Node<T, Order>* Current = this;  // current point to root
+    Node<T, Order>* Current = this;
     while (true) {
-        // is leaf
         if (Current->childs[0] == nullptr) {
             return COUNT;
         }
@@ -152,7 +137,6 @@ int Node<T, Order>::getHeight() {
     }
 }
 
-// Deallocation
 template <class T, int Order>
 Node<T, Order>::~Node() {
     delete[] keys;
@@ -161,7 +145,6 @@ Node<T, Order>::~Node() {
     delete[] childs;
 }
 
-// Serialización
 template <class T, int Order>
 void Node<T, Order>::serialize(ofstream& out) const {
     out << NumbersOfKeys << ' ' << position << ' ';
@@ -170,10 +153,10 @@ void Node<T, Order>::serialize(ofstream& out) const {
     }
     for (int i = 0; i <= NumbersOfKeys; ++i) {
         if (childs[i]) {
-            out << 1 << ' ';  // indicates that the child exists
+            out << 1 << ' ';
             childs[i]->serialize(out);
         } else {
-            out << 0 << ' ';  // indicates that the child is nullptr
+            out << 0 << ' ';
         }
     }
 }
@@ -196,9 +179,25 @@ void Node<T, Order>::deserialize(ifstream& in) {
     }
 }
 
-/////////////////////////////////////////////////////////////
+template <class T, int Order>
+void Node<T, Order>::to_dot(std::ostringstream& oss, int& nodeCounter) const {
+    int currentId = nodeCounter++;
+    oss << "node" << currentId << " [label=\"";
+    for (int i = 0; i < NumbersOfKeys; ++i) {
+        if (i > 0) oss << "|";
+        oss << "<f" << i << ">" << keys[i];
+    }
+    oss << "\"];\n";
+    
+    for (int i = 0; i <= NumbersOfKeys; ++i) {
+        if (childs[i]) {
+            int childId = nodeCounter;
+            childs[i]->to_dot(oss, nodeCounter);
+            oss << "node" << currentId << ":f" << i << " -> node" << childId << ";\n";
+        }
+    }
+}
 
-// BTree protoType Function
 template <class T, int Order>
 class BTree {
 private:
@@ -213,11 +212,9 @@ public:
 
     void serialize(const string& filename) const;
     void deserialize(const string& filename);
+    void to_dot(std::ostringstream& oss) const;
 };
 
-/////////////////////////////////////////////////////////////
-
-// BTree implementation
 template <class T, int Order>
 BTree<T, Order>::BTree() {
     this->Root = nullptr;
@@ -226,12 +223,11 @@ BTree<T, Order>::BTree() {
 template <class T, int Order>
 void BTree<T, Order>::Insert(T value) {
     count++;
-    // if Tree is empty
     if (this->Root == nullptr) {
         this->Root = new Node<T, Order>();
         this->Root->keys[++this->Root->position] = value;
         this->Root->NumbersOfKeys = 1;
-    } else {  // if tree not empty
+    } else {
         int check = Root->Insert(value);
         if (check) {
             T mid;
@@ -248,49 +244,56 @@ void BTree<T, Order>::Insert(T value) {
 
 template <class T, int Order>
 void BTree<T, Order>::Print() const {
-    if (Root != nullptr)
-        Root->Print();
-    else
-        cout << "The B-Tree is Empty" << endl;
+    if (this->Root) this->Root->Print();
 }
 
 template <class T, int Order>
 BTree<T, Order>::~BTree() {
-    delete Root;
+    delete this->Root;
 }
 
-// Serialización
 template <class T, int Order>
 void BTree<T, Order>::serialize(const string& filename) const {
     ofstream out(filename);
-    if (out.is_open()) {
-        if (Root) {
-            out << 1 << ' ';  // indicates that the root exists
-            Root->serialize(out);
-        } else {
-            out << 0;  // indicates that the tree is empty
-        }
-        out.close();
-    } else {
-        cerr << "Unable to open file for writing: " << filename << endl;
-    }
+    Root->serialize(out);
+    out.close();
 }
 
-// Deserialización
 template <class T, int Order>
 void BTree<T, Order>::deserialize(const string& filename) {
     ifstream in(filename);
-    if (in.is_open()) {
-        int rootExists;
-        in >> rootExists;
-        if (rootExists) {
-            Root = new Node<T, Order>();
-            Root->deserialize(in);
-        }
-        in.close();
-    } else {
-        cerr << "Unable to open file for reading: " << filename << endl;
+    Root = new Node<T, Order>();
+    Root->deserialize(in);
+    in.close();
+}
+
+template <class T, int Order>
+void BTree<T, Order>::to_dot(std::ostringstream& oss) const {
+    oss << "digraph G {\nnode [shape=record];\n";
+    int nodeCounter = 0;
+    if (Root) {
+        Root->to_dot(oss, nodeCounter);
+    }
+    oss << "}\n";
+}
+
+extern "C" {
+    BTree<int, 3>* create_tree() {
+        return new BTree<int, 3>();
+    }
+
+    void Insert(BTree<int, 3>* tree, int value) {
+        tree->Insert(value);
+    }
+
+    void delete_tree(BTree<int, 3>* tree) {
+        delete tree;
+    }
+
+    const char* to_dot(BTree<int, 3>* tree) {
+        std::ostringstream oss;
+        tree->to_dot(oss);
+        return strdup(oss.str().c_str());
     }
 }
 
-#endif // BTREE_H
--- README.md
+++
@@ -1,5 +0,0 @@
-Comandos en su directorio:
-
-1. npm init -y;
-
-2. npm install express;
